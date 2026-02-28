b66b5ecd0c5d1ae19a16a28569a79ae75ce485c3
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2026-02-28 19:04:02 +0530

    registration backend

--- March_practice_folder_backend/01_django_start/backend-drf/stock_prediction_main/settings.py
+++ March_practice_folder_backend/01_django_start/backend-drf/stock_prediction_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b%ftn+%v(tu5_pnc@vctqiex=$@*9we4a7tslw=na@2enydvdl'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
+
+EXTERNAL_APPS = [
+    'accounts',
+    'api'
+]
+
+INSTALLED_APPS += EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
